89d5acf899771bedd9cac6731248b21345708351
Jake Dobler <jaecob.d1@gmail.com>
2022-07-30 14:18:14 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Systems_of_linear_equations_solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xValue1 = new System.Windows.Forms.TextBox();
+            this.yValue1 = new System.Windows.Forms.TextBox();
+            this.xValue2 = new System.Windows.Forms.TextBox();
+            this.yValue2 = new System.Windows.Forms.TextBox();
+            this.Equals2 = new System.Windows.Forms.TextBox();
+            this.equals1 = new System.Windows.Forms.TextBox();
+            this.EqualsSign = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Solutions = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // xValue1
+            // 
+            this.xValue1.Location = new System.Drawing.Point(239, 123);
+            this.xValue1.Name = "xValue1";
+            this.xValue1.PlaceholderText = "(x)";
+            this.xValue1.Size = new System.Drawing.Size(56, 23);
+            this.xValue1.TabIndex = 0;
+            // 
+            // yValue1
+            // 
+            this.yValue1.Location = new System.Drawing.Point(364, 123);
+            this.yValue1.Name = "yValue1";
+            this.yValue1.PlaceholderText = "(y)";
+            this.yValue1.Size = new System.Drawing.Size(56, 23);
+            this.yValue1.TabIndex = 1;
+            // 
+            // xValue2
+            // 
+            this.xValue2.Location = new System.Drawing.Point(239, 198);
+            this.xValue2.Name = "xValue2";
+            this.xValue2.PlaceholderText = "(x2)";
+            this.xValue2.Size = new System.Drawing.Size(56, 23);
+            this.xValue2.TabIndex = 2;
+            // 
+            // yValue2
+            // 
+            this.yValue2.Location = new System.Drawing.Point(364, 198);
+            this.yValue2.Name = "yValue2";
+            this.yValue2.PlaceholderText = "(y2)";
+            this.yValue2.Size = new System.Drawing.Size(56, 23);
+            this.yValue2.TabIndex = 3;
+            // 
+            // Equals2
+            // 
+            this.Equals2.Location = new System.Drawing.Point(482, 198);
+            this.Equals2.Name = "Equals2";
+            this.Equals2.PlaceholderText = "(Equals)";
+            this.Equals2.Size = new System.Drawing.Size(56, 23);
+            this.Equals2.TabIndex = 4;
+            // 
+            // equals1
+            // 
+            this.equals1.Location = new System.Drawing.Point(468, 123);
+            this.equals1.Name = "equals1";
+            this.equals1.PlaceholderText = "(Equals)";
+            this.equals1.Size = new System.Drawing.Size(56, 23);
+            this.equals1.TabIndex = 5;
+            // 
+            // EqualsSign
+            // 
+            this.EqualsSign.AutoSize = true;
+            this.EqualsSign.Location = new System.Drawing.Point(438, 126);
+            this.EqualsSign.Name = "EqualsSign";
+            this.EqualsSign.Size = new System.Drawing.Size(15, 15);
+            this.EqualsSign.TabIndex = 6;
+            this.EqualsSign.Text = "=";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(438, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "=";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(611, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Find Solution";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Solutions
+            // 
+            this.Solutions.FormattingEnabled = true;
+            this.Solutions.ItemHeight = 15;
+            this.Solutions.Location = new System.Drawing.Point(225, 271);
+            this.Solutions.Name = "Solutions";
+            this.Solutions.Size = new System.Drawing.Size(313, 124);
+            this.Solutions.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(325, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(325, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "+";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(611, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Parse from File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Solutions);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EqualsSign);
+            this.Controls.Add(this.equals1);
+            this.Controls.Add(this.Equals2);
+            this.Controls.Add(this.yValue2);
+            this.Controls.Add(this.xValue2);
+            this.Controls.Add(this.yValue1);
+            this.Controls.Add(this.xValue1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox xValue1;
+        private System.Windows.Forms.TextBox yValue1;
+        private System.Windows.Forms.TextBox xValue2;
+        private System.Windows.Forms.TextBox yValue2;
+        private System.Windows.Forms.TextBox Equals2;
+        private System.Windows.Forms.TextBox equals1;
+        private System.Windows.Forms.Label EqualsSign;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox Solutions;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using System.IO;
+
+namespace Systems_of_linear_equations_solver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            char[] delimiterChars = { ' ', ',', '.', ':', '=', '\t' };
+            //Call Variables
+            int[] Equation1 =
+            {
+                Int32.Parse(xValue1.Text),
+                Int32.Parse(yValue1.Text),
+             };
+            int[] Equation2 =
+            {
+                Int32.Parse(xValue2.Text),
+                Int32.Parse(yValue2.Text),
+            };
+
+            int[][] matrix1 = new int[][]
+            {
+                Equation1,
+                Equation2,
+            };
+
+            int[] matrixAnswers = new int[]
+            {
+                Int32.Parse(equals1.Text),
+                Int32.Parse(Equals2.Text),
+            };
+
+            int[][] matrixB = new int[2][];
+            matrixB[0] = new int[]
+            {
+                matrix1[1][1],
+                (matrix1[0][1] * -1),
+            };
+            matrixB[1] = new int[]
+            {
+                (matrix1[1][0] * -1),
+                matrix1[0][0],
+            };
+
+            String res = "nothing yet";
+            int A;
+
+
+
+            if (matrix1[0][1] == matrix1[1][1])
+            {
+                res = "Lines are Paralel";
+            }
+            else
+            {
+                int[][] InverseMatrix = new int[2][];
+
+                InverseMatrix[0] = new int[2] { 0, 0 };
+                InverseMatrix[1] = new int[2] { 0, 0 };
+                A = ((matrix1[0][0] * matrix1[1][1]) - (matrix1[0][1] * matrix1[1][0]));
+                for (int i = 0; i < matrixB.Length; i++)
+                {
+                    for (int j = 0; j < matrixB[i].Length; j++)
+                    {
+                        InverseMatrix[i][j] = (matrixB[i][j]);
+                        //Solutions.Items.Add()
+
+                        Console.WriteLine(InverseMatrix[i][j]);
+
+
+
+                    }
+                }
+                int x = (((InverseMatrix[0][0] * matrixAnswers[0]) + (InverseMatrix[0][1] * matrixAnswers[1])) / A);
+                int y = (((InverseMatrix[1][0] * matrixAnswers[0]) + (InverseMatrix[1][1] * matrixAnswers[1])) / A);
+                res = $"{matrix1[0][0]}x + {matrix1[0][1]}y = {matrixAnswers[0]} & \n " +
+                    $"{matrix1[1][0]}x + {matrix1[1][0]}y = {matrixAnswers[1]} | \n " +
+                    $"x={x}, y={y}";
+
+            }
+
+
+
+
+
+            Solutions.Items.Add(res);
+
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+            {
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "txt files (*.txt)|*.txt|All Files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog1.FileName;
+                    var fileStream = openFileDialog1.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        List<string> lines = File.ReadAllLines(filePath).ToList();
+                       foreach(var line in lines)
+                        {
+                            string[] entries = line.Split(',');
+
+                            xValue1.Text = entries[0];
+                            yValue1.Text = entries[1];
+                            equals1.Text = entries[2];
+                            xValue2.Text = entries[3];
+                            yValue2.Text = entries[4];
+                            Equals2.Text = entries[5];
+
+                        }
+
+                    }
+
+
+                }
+                MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+
+            }
+
+        }
+
+    }
+
+}
